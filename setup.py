9bf02bb5326f0c2f4a5fed71f6e90fbce296e8d8
BloggingKIng <huzaifablogger.contact@gmail.com>
2022-08-26 13:05:51 +0500

    Added all the package files a documentation to be added soon

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+
+setuptools.setup(
+
+    name="CovidDataApi",
+    version="1.0.0",
+    author="Huzaifa Azhar",
+    description="A simple python package to get Covid Stats just by using simple commands!",
+    install_requires=["bs4"]
+)
