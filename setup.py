f422748d21715ce6f5688d9e99aa8365b2d01838
BloggingKIng <huzaifablogger.contact@gmail.com>
2022-09-15 12:46:46 +0500

    Maked the Markdown description work on PyPi 3 minor versions released

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
 import setuptools
 
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setuptools.setup(
 
     name="GetCovidData",
-    version="1.0.0",
+    version="1.0.4",
     author="Huzaifa Azhar",
     description="A simple python package to get Covid Stats just by using simple commands!",
+    readme = "README.md",
+    long_description=long_description,
+     long_description_content_type='text/markdown',
     install_requires=["bs4"]
 )
